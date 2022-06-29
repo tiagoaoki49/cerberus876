f78836d16a23a75d150af35e614e9424b67ab072
nuno.passos <nuno.passos@ibanwallet.com>
2022-06-29 18:55:36 +0100

    Creating an array

--- cerberus/src/main/java/br/com/letscode/App.java
+++ cerberus/src/main/java/br/com/letscode/App.java
@@ -1,13 +1,15 @@
 package br.com.letscode;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.util.Random;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+
+        int[] t = new int[5];
+        for (int i = 0; i< t.length; i++) {
+            t[i] = new Random().nextInt() % 500;
+        }
     }
+
 }
