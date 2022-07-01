d63d4d3782d7c27b86378dd862e26c753315e791
F8236816 Pedro Faustino de Oliveira Rodrigues de Melo <pedro.faustino@bb.com.br>
2022-07-01 17:28:32 -0300

    CLasse pessoa

--- cerberus/src/main/java/br/com/letscode/App.java
+++ cerberus/src/main/java/br/com/letscode/App.java
@@ -4,10 +4,11 @@ package br.com.letscode;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // Criar uma classe Pessoa, com atributos:
+        // nome, idade, sexo, morada, estado civil, altura, peso
+
     }
 }
