bd7c9e10529916b6f27cf683be90bfa453c25127
F3879710 Gustavo de Magalhaes Barbosa <gustavo.jay@bb.com.br>
2022-07-01 18:36:30 -0300

    getters e setters para a pessoa

--- cerberus/src/main/java/br/com/letscode/modelos/Pessoa.java
+++ cerberus/src/main/java/br/com/letscode/modelos/Pessoa.java
@@ -6,4 +6,35 @@ public class Pessoa {
 	private EstadoCivil estadoCivil;
 	private Genero genero;
 	private String profissao;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public EstadoCivil getEstadoCivil() {
+		return estadoCivil;
+	}
+	public void setEstadoCivil(EstadoCivil estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+	public Genero getGenero() {
+		return genero;
+	}
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+	public String getProfissao() {
+		return profissao;
+	}
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
 }
