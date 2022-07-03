8b9263ae0e28852c8c4f07d2b41277d54401858d
nuno.passos <nuno.passos@ibanwallet.com>
2022-07-03 19:31:19 +0100

    Cleaned the main method

--- cerberus/src/main/java/br/com/letscode/App.java
+++ cerberus/src/main/java/br/com/letscode/App.java
@@ -1,53 +1,10 @@
 package br.com.letscode;
 
-import java.util.Arrays;
-import java.util.Random;
-
 import br.com.letscode.veiculos.Veiculo;
 
 public class App
 {
     public static void main(String[] args) {
-        String str = "Isto é uma String";
-        char[] charArray = str.toCharArray();
-
-        // foreach
-        // Não permite a alteração do array
-/*
-        for (char c: charArray) {
-            if (c == ' ') {
-                c = '_';
-            }
-            System.out.print(c);
-        }
- */
-        String[] strArray = {"str1", "str2", "str3"};
-
-        // Arrays.stream(strArray).forEach(System.out::println);
-        // Arrays.stream(strArray).forEach(s -> System.out.println(s));
-
-        // pode não percorrer todos os elementos
-        // permite alterar o array
-        for (int i = 0; i < charArray.length; i+=2) {
-            if (charArray[i] == ' ') {
-                charArray[i] = '_';
-            }
-            // System.out.print(c);
-        }
-
-        // criação de uma matriz
-        int[][] matrix = Utils.populateMatrix(5, 6);
-        Utils.printMatrix(matrix);
-        Utils.orderMatrix(matrix);
-        System.out.println();
-        Utils.printMatrix(matrix);
-
-
-        int[] array = Utils.populateArray(5);
-        Arrays.stream(array).forEach(s -> System.out.print(s + " "));
-        System.out.println();
-        Utils.orderArray(array);
-        Arrays.stream(array).forEach(s -> System.out.print(s + " "));
 
         Veiculo v = new Veiculo();
         v.setCor("Vermelho");
